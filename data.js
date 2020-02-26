c178ee5c9c3bc97988fa7f552c99680b9231b45c
Reece Bryant <reececb@gmail.com>
2020-02-26 01:24:40 -0600

    Initial Commit

---
+++ data.js
@@ -0,0 +1,58 @@
+const STORE = {
+    questions: [
+       {
+         question: "What does the acronym, \"CPU\" represent?",
+         choices: [
+           "Critical Processing Unit",
+           "Connective Processing Umbrella",
+           "Central Processing Unit",
+           "Conformative Protective Uncertainty"
+         ],
+       answer: "Central Processing Unit"
+     },
+     {
+       question: "What unit of measurement is used for monitor refresh rates?",
+       choices: [
+         "Hertz(Hz)",
+         "MegaHertz(MHz)",
+         "Watts",
+         "Clocks"
+       ],
+       answer: "Hertz(Hz)"
+     },
+     {
+       question: "Which of the following is an operating system?",
+       choices: [
+         "Umbrella",
+         "ASUS",
+         "Lenovo",
+         "Windows"
+       ],
+       answer: "Windows"
+     },
+     {
+       question: "What does the acronym, \"RAM\" represent?",
+       choices: [
+         "Random Accumlative Management",
+         "Radical Access Memory",
+         "Reasonable Amorphous Metabolite",
+         "Random Access Memory"
+       ],
+       answer: "Random Access Memory"
+     },
+     {
+       question: "How many pins does a standard 12V ATX power supply connector on a motherboard have?",
+       choices: [
+         "24",
+         "3",
+         "16",
+         "10"
+       ],
+       answer: "24"
+     }
+   ],
+   questionNumber: 0,
+   score: 0
+ };
+ 
+ 
